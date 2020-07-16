ac25547c3ec3df738b076e641860c68602256ca6
Marc <noviluni@gmail.com>
2020-07-16 10:13:39 +0200

    improve clevert and README

--- README.md
+++ README.md
@@ -8,12 +8,18 @@ To use it:
 crawl tools4noobs.com -a max_number=100 -a locale=en_US -o numbers_en_US.csv
 ```
 
+or
+
+```bash
+scrapy crawl clevert.com.br -a max_number=100 -a locale=es -o numbers_es.csv
+```
+
 You can set the `max_number` to reach, the desired `locale` and the name of the result file. You can also add an optional `start_number` parameter.
 
 The produced CSV file can be sorted by using the `sort_csv` function. Example:
 
 ```python
-from get_numbers.utils import sort_csv                                                                                                                                             
+from get_numbers.utils import sort_csv
 
 sort_csv('numbers_en_US.csv')
 ```
--- get_numbers/spiders/clevert_com_br.py
+++ get_numbers/spiders/clevert_com_br.py
@@ -8,27 +8,69 @@ from get_numbers.spiders import BaseNumberSpider
 class ClevertComBrSpider(BaseNumberSpider):
     name = 'clevert.com.br'
     allowed_domains = ['clevert.com.br']
-    start_urls = ['http://clevert.com.br/']
+    supported_locales = [
+        'af',
+        'sq',
+        'ar',
+        'hy',
+        'az',
+        'be',
+        'bg',
+        'ca',
+        'zh',
+        'hr',
+        'cs',
+        'da',
+        'nl',
+        'en',
+        'eo',
+        'et',
+        'mk',
+        'fo',
+        'fa',
+        'fi',
+        'fr',
+        'ka',
+        'de',
+        'el',
+        'he',
+        'hi',
+        'hu',
+        'is',
+        'id',
+        'it',
+        'ja',
+        'ko',
+        'lv',
+        'lt',
+        'ms',
+        'mt',
+        'nb',
+        'nn',
+        'pl',
+        'pt-br',
+        'ro',
+        'ru',
+        'sr',
+        'sk',
+        'sl',
+        'es',
+        'sv',
+        'ta',
+        'th',
+        'tr',
+        'uk',
+        'vi',
+        'cy',
+    ]
 
     def crawl_numbers(self, start_number, max_number, locale):
         url = 'https://clevert.com.br/t/en/numbers_to_words/generate'
 
         headers = {
-            # "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:78.0) Gecko/20100101 Firefox/78.0",
-            # "Accept": "*/*",
-            # "Accept-Language": "ca,en-US;q=0.7,en;q=0.3",
             "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
             "X-Requested-With": "XMLHttpRequest",
-            # "Origin": "https://clevert.com.br",
-            # "Connection": "keep-alive",
-            # "Referer": "https://clevert.com.br/t/en/numbers-to-words",
-            # "Pragma": "no-cache",
-            # "Cache-Control": "no-cache"
         }
-
-        # cookies = {
-        #     "ci_session": "r1iefqp2g4u0d704blfmiid77u6tfln3"
-        # }
 
         for number in range(start_number, int(max_number) + 1):
             query_params = {
